760c3d05c3ab0415c44899315403c7066b1c0c3a
Tarannummm <tarannumelina@gmail.com>
2025-10-29 23:26:30 +0600

    operator check

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 
-# Program to check if a year is a leap year
+# # Program to check if a year is a leap year
 
-year = int(input("Enter a year: "))
+# year = int(input("Enter a year: "))
 
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print("Leap Year")
-else:
-    print("Not a Leap Year")
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("Leap Year")
+# else:
+#     print("Not a Leap Year")
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /): ")
+if op == '+': print(a + b)
+elif op == '-': print(a - b)
+elif op == '*': print(a * b)
+elif op == '/': print(a / b)
+else: print("Invalid operator")
