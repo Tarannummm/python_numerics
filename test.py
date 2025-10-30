eea4f625142d8030367d028bff868652ef967af1
Tarannummm <tarannumelina@gmail.com>
2025-10-30 23:41:22 +0600

    factorial

--- test.py
+++ test.py
@@ -9,11 +9,18 @@
 #     print("Not a Leap Year")
 
 
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-op = input("Enter operator (+, -, *, /): ")
-if op == '+': print(a + b)
-elif op == '-': print(a - b)
-elif op == '*': print(a * b)
-elif op == '/': print(a / b)
-else: print("Invalid operator")
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: "))
+# op = input("Enter operator (+, -, *, /): ")
+# if op == '+': print(a + b)
+# elif op == '-': print(a - b)
+# elif op == '*': print(a * b)
+# elif op == '/': print(a / b)
+# else: print("Invalid operator")
+
+
+n = int(input("Enter number: "))
+fact = 1
+for i in range(1, n + 1):
+fact *= i
+print("Factorial =", fact)
