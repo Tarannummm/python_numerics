4d79e06dfe239bef2d373f10d4b4d61748bcee6b
Tarannummm <tarannumelina@gmail.com>
2025-11-01 23:53:16 +0600

    secant

---
+++ Secant Method.py
@@ -0,0 +1,13 @@
+import math
+
+f=lambda x: math.exp(-x)-x
+a=0
+b=1
+iter=1
+
+while iter<=5:
+    c = (a*f(b) - b*f(a)) / (f(b)-f(a))
+    print(" Iter: %d  App_root: %0.5f"%(iter,c))
+    a=b
+    b=c
+    iter=iter+1
