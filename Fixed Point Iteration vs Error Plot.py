4a6248b807c10aeb46645f385f366b67757d999c
Tarannummm <tarannumelina@gmail.com>
2025-11-02 22:46:15 +0600

    fpi vs error plot

---
+++ Fixed Point Iteration vs Error Plot.py
@@ -0,0 +1,19 @@
+import math
+import matplotlib.pyplot as plt
+
+root = []
+errors = []
+g = lambda x: math.exp(-x)
+x0 = 0
+
+for i in range(15):
+    x1 = g(x0)
+    root.append(x1)
+    error = abs(x1 - x0)
+    errors.append(error)
+    x0 = x1
+
+plt.plot(root, label='Root')
+plt.plot(errors, label='Error')
+plt.legend()
+plt.show()
