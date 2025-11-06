87a581de3ae50b6d2cd0b6805203dd23a2a2d265
Tarannummm <tarannumelina@gmail.com>
2025-11-06 23:23:39 +0600

    even odd

--- test.py
+++ test.py
@@ -19,8 +19,17 @@
 # else: print("Invalid operator")
 
 
-n = int(input("Enter number: "))
-fact = 1
-for i in range(1, n + 1):
-fact *= i
-print("Factorial =", fact)
+# n = int(input("Enter number: "))
+# fact = 1
+# for i in range(1, n + 1):
+# fact *= i
+# print("Factorial =", fact)
+
+
+
+x = 10
+if x > 0:
+    if x % 2 == 0:
+        print("Positive Even")
+    else:
+        print("Positive Odd")
