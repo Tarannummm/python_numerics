1307ce65edfe308b473312e0d587fd924d8631e8
Tarannummm <tarannumelina@gmail.com>
2025-11-13 23:49:58 +0600

    seidel

--- Gauss-Jacobi.py
+++ Gauss-Jacobi.py
@@ -1,12 +1,38 @@
+
+
+# 3x+y+z=5
+# −2x+5y−z=−20
+# x+y+7z=10
+
+
+
+
+import matplotlib.pyplot as plt
 a = 0 #x
 b = 0 #y
 c = 0 #z
-
+ 
+solx=[]
+soly=[]
+solz=[] 
 for i in range(20):
     x = (5-b-c)/3
     y = (2*a+c-20)/5
     z = (10-a-b)/7
+    solx.append(x)
+    soly.append(y)
+    solz.append(z)
     a = x
     b = y
     c = z
-    print("Iter=%d x = %0.2f y=%0.2f z = %0.2f"%(i+1,x,y,z))
+
+plt.plot(solx)
+plt.plot(soly)
+plt.plot(solz)
+    
+plt.show()
+plt.xlabel('Iteration')
+plt.ylabel('Solution')
+plt.title('Jacobi Iteration Convergence')
+
+plt.legend()
--- Gauss-Seidel.py
+++ Gauss-Seidel.py
@@ -8,3 +8,30 @@ for i in range(10):
     z = (10-x-y)/7
   
     print("Iter=%d x = %0.2f y=%0.2f z = %0.2f"%(i+1,x,y,z))
+
+
+
+import matplotlib.pyplot as plt
+
+#2x + y = 3, x - 3y = -5
+
+x = 0
+y = 0
+
+solX=[]
+solY = []
+for i in range(6):
+    x = (3-y)/2
+    y = (x+5)/3
+    solX.append(x)
+    solY.append(y)
+
+plt.plot(solX)
+plt.plot(solY)
+
+plt.xlabel('Iteration')
+plt.ylabel('Solution')
+
+plt.legend()
+plt.show()
+
