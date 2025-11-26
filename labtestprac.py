68d98360e3195a30e25ae55ba44d2770f302671b
Tarannummm <tarannumelina@gmail.com>
2025-11-27 04:10:13 +0600

    lagrange

--- labtestprac.py
+++ labtestprac.py
@@ -141,14 +141,31 @@
 # plt.plot(errors, label='Error')
 # plt.legend()
 # plt.show()
-              
-x = [1, 2, 3, 4]
-y = [5, 6, 7, 8]
+  #linear spline            
+# x = [1, 2, 3, 4]
+# y = [5, 6, 7, 8]
 
-n = len(x)
-xx = 2.5
+# n = len(x)
+# xx = 2.5
 
-for i in range(n - 1):
-    if (xx >= x[i]) and (xx <= x[i + 1]):
-        S = y[i] + (y[i + 1] - y[i]) * (xx - x[i]) / (x[i + 1] - x[i])
-        print("Interpolated value:", S)
+# for i in range(n - 1):
+#     if (xx >= x[i]) and (xx <= x[i + 1]):
+#         S = y[i] + (y[i + 1] - y[i]) * (xx - x[i]) / (x[i + 1] - x[i])
+#         print("Interpolated value:", S)
+
+
+
+
+x=[1,2,3]
+y=[4,5,6]
+xx=3
+n=len(x)
+L=0
+for i in range(n):
+    li=1
+    for j in range(n):
+        if (i!=j):
+            li *=(xx-x[j])/x[i]-x[j]
+    L +=y[i]*li
+    print(L)
+
