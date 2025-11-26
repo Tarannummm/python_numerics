ca5455b9dcdbd9d77533cc1236eb912c442c2996
Tarannummm <tarannumelina@gmail.com>
2025-11-27 02:51:48 +0600

    false position

--- labtestprac.py
+++ labtestprac.py
@@ -41,28 +41,60 @@
 # 2x + 10y + 3z = 19
 # x + 3y + 10z = 0
 
-import math
+# import math
+# import matplotlib.pyplot as plt
+# a=0
+# b=1
+# c=0
+# solx=[]
+# soly=[]
+# solz=[]
+# for i in range(5):
+#     x=(9-2*b-c)/10
+#     y=(19-2*a-3*c)/10
+#     z=(-a-3*b)/10
+#     solx.append(x)
+#     soly.append(y)
+#     solz.append(z)
+#     a=x
+#     b=y
+#     c=z
+#     print(x,y,z)
+# plt.plot(solx,label='x')
+# plt.plot(soly,label='y')
+# plt.plot(solz,label='z')
+# plt.legend()
+# plt.grid()
+# plt.show()
+
+# Use the False Position (Regula Falsi) method to find the root of the equation:
+# x3−4x−9=0
+# Take the initial guesses 
+#  a=2 and b=3.
+
+# Use a tolerance of 
+
+# 0.0001.
+
+# Perform iterations until the approximate root satisfies the tolerance.
+
+# At each iteration, print the approximate root and the error.
+
+import math 
 import matplotlib.pyplot as plt
-a=0
-b=1
-c=0
-solx=[]
-soly=[]
-solz=[]
-for i in range(5):
-    x=(9-2*b-c)/10
-    y=(19-2*a-3*c)/10
-    z=(-a-3*b)/10
-    solx.append(x)
-    soly.append(y)
-    solz.append(z)
-    a=x
-    b=y
-    c=z
-    print(x,y,z)
-plt.plot(solx,label='x')
-plt.plot(soly,label='y')
-plt.plot(solz,label='z')
-plt.legend()
-plt.grid()
-plt.show()
+f=lambda x:x**3-4*x-9
+a=2
+b=3
+r=2.708375
+tol=0.0001
+for i in range(1,101):
+    c=(a*f(b)-b*f(a))/(f(b)-f(a))
+    err=abs(c-r)
+    print(c)
+    if err<tol:
+        break
+    if f(c)>0:
+        b=c
+    else:
+        a=c
+          
