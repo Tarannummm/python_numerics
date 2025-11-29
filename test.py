bb8f515fb2c9f738057deab386ef6d28ec38dd96
Tarannummm <tarannumelina@gmail.com>
2025-11-29 22:35:13 +0600

    quick sort

--- test.py
+++ test.py
@@ -175,17 +175,31 @@
 #     if prime:
 #         print(num, end=" ")
 
+#insertion sort
+# arr = [9, 5, 1, 4, 3]
 
-arr = [9, 5, 1, 4, 3]
-
-for i in range(1, len(arr)):
-    key = arr[i]
-    j = i - 1
+# for i in range(1, len(arr)):
+#     key = arr[i]
+#     j = i - 1
     
-    while j >= 0 and arr[j] > key:
-        arr[j + 1] = arr[j]
-        j -= 1
+#     while j >= 0 and arr[j] > key:
+#         arr[j + 1] = arr[j]
+#         j -= 1
 
-    arr[j + 1] = key
+#     arr[j + 1] = key
 
-print(arr)
+# print(arr)
+
+#quick sort
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr)//2]
+    left = [x for x in arr if x < pivot]
+    mid  = [x for x in arr if x == pivot]
+    right= [x for x in arr if x > pivot]
+
+    return quick_sort(left) + mid + quick_sort(right)
+
+print(quick_sort([6, 3, 8, 5, 2, 7, 4]))
