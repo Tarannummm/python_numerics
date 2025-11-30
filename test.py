2ec10e98725a22e7a960ff76a33ff29cb28d3898
Tarannummm <tarannumelina@gmail.com>
2025-11-30 23:50:52 +0600

    merge sort

--- test.py
+++ test.py
@@ -191,15 +191,51 @@
 # print(arr)
 
 #quick sort
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
+# def quick_sort(arr):
+#     if len(arr) <= 1:
+#         return arr
     
-    pivot = arr[len(arr)//2]
-    left = [x for x in arr if x < pivot]
-    mid  = [x for x in arr if x == pivot]
-    right= [x for x in arr if x > pivot]
+#     pivot = arr[len(arr)//2]
+#     left = [x for x in arr if x < pivot]
+#     mid  = [x for x in arr if x == pivot]
+#     right= [x for x in arr if x > pivot]
 
-    return quick_sort(left) + mid + quick_sort(right)
+#     return quick_sort(left) + mid + quick_sort(right)
 
-print(quick_sort([6, 3, 8, 5, 2, 7, 4]))
+# print(quick_sort([6, 3, 8, 5, 2, 7, 4]))
+
+#merge sort
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+arr = [9, 4, 7, 3, 1, 6]
+merge_sort(arr)
+print(arr)
+
