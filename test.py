f774e8b7cb6dac5ed0a3cb8f7e57f5a6a493b851
Tarannummm <tarannumelina@gmail.com>
2025-12-05 23:33:06 +0600

    queue

--- test.py
+++ test.py
@@ -253,17 +253,17 @@
 # print("Final Stack:", stack)
 
 
+#queue
 
-import math
-f=lambda x:math.exp(x)
-a=0
-b=1
-n=4
-h=(b-a)/n
-sum=f(a)+f(b)
-for i in range(n-1):
-    a=a+h
-    sum=sum+2*f(a)
-sum=sum*(h/2)
-print(sum)
+queue = []
 
+# enqueue
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+# dequeue
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("Final Queue:", queue)
