cb5508e0ce67f1a75f678954abb19227ed7ea10d
Tarannummm <tarannumelina@gmail.com>
2025-12-26 20:57:28 +0600

    simspons 1/3

--- labfinal-prac.py
+++ labfinal-prac.py
@@ -12,3 +12,22 @@ for i in range(1,n):
     sum=sum+2*f(a)
 sum=sum*(h/2)
 print(sum)
+
+
+#simpson's 1/3 
+#f=x**2+1 
+import math
+f=lambda x:x**2+1
+a=0
+b=12
+n=6
+h=(b-a)/n
+sum=f(a)+f(b)
+for i in range(1,n):
+    a=a+h
+    if i%2==0:
+        sum=sum+2*f(a)
+    else:
+        sum=sum+4*f(a)
+sum=sum*(h/3)
+print(sum)
